c1dbaee1daaac10015ea35f8670a877ebb31fe94
ilmeedesilva <ilmeedesilva@gmail.com>
2024-03-11 17:41:00 +0530

    created donation history service and controller methods

--- src/main/java/com/bcn/donorService/service/DonationHistoryService.java
+++ src/main/java/com/bcn/donorService/service/DonationHistoryService.java
@@ -1,12 +1,20 @@
 package com.bcn.donorService.service;
 
+import com.bcn.donorService.data.DonationHistory;
 import com.bcn.donorService.data.DonationHistoryRepository;
+import com.bcn.donorService.data.Donor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DonationHistoryService {
 
     @Autowired
     private DonationHistoryRepository donationHistoryRepository;
+
+    public List<DonationHistory> findDonationByNic(String donorNic){
+        return donationHistoryRepository.findDonationByNic(donorNic);
+    }
 }
